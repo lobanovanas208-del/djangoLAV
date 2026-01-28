2bbdb3109d5e8c72a40b9ec7ca3da2ea649e95d5
kabinet303 <kabinet303@localhost.localdomain>
2026-01-28 11:42:18 +0300

    создание проекта

---
+++ SiteLAV/mainLAV/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MainlavConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mainLAV'
---
+++ SiteLAV/mainLAV/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def mainfLAV (request):
+    return HttpResponse("app mainLAV")
+
+# Create your views here.
